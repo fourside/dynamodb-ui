bbee9b2790d2d907105544d1915b3d3884176cac
fourside <fourside@gmail.com>
2020-01-14 22:31:17 +0900

    dynamodb client, scan and list tables

---
+++ src/client/client.spec.ts
@@ -0,0 +1,15 @@
+import { Client } from "./client";
+
+describe("client", () => {
+
+  beforeEach(() => {
+  });
+
+  it("listTable return table names", async () => {
+    const client = new Client();
+    const tables = await client.listTablesByEnv();
+    //console.log(tables);
+    expect(tables.develop.length).not.toBe(0);
+  });
+
+});
---
+++ src/client/client.ts
@@ -0,0 +1,58 @@
+import AWS from "aws-sdk";
+import DynamoDB, { DocumentClient, TableNameList } from "aws-sdk/clients/dynamodb";
+
+AWS.config.update({
+  region: "us-west-2",
+});
+
+export class Client {
+  dynamoDb :DynamoDB;
+  client :DocumentClient;
+  tableNamePattern = /^([a-zA-Z]+)-([a-z0-9]+)-([a-z]+)$/;
+
+  constructor() {
+    this.dynamoDb = new AWS.DynamoDB();
+    this.client = new AWS.DynamoDB.DocumentClient();
+  }
+
+  async scan(tableName :string) {
+    return new Promise((resolve, reject) => {
+      this.client.scan({ TableName: tableName}, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      })
+    });
+  }
+
+  async listTablesByEnv() :Promise<TableList> {
+    const tables = await new Promise<TableNameList>((resolve, reject) => {
+      this.dynamoDb.listTables({}, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data.TableNames);
+        }
+      });
+    });
+    const tableListList :TableList = {};
+    return tables.reduce((accumelator, table) => {
+      const matches = this.tableNamePattern.exec(table);
+      if (!matches?.[3]) {
+        return accumelator;
+      }
+      const env = matches[3];
+      if (!accumelator[env]) {
+        accumelator[env] = [];
+      }
+      accumelator[env].push(table);
+      return accumelator;
+    }, tableListList)
+  }
+}
+
+export interface TableList {
+  [envName :string] : string[]
+};
