92f5da7c06c760608167d27b08e19d6fce58372e
fourside <fourside@gmail.com>
2020-01-18 16:23:49 +0900

    add eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+      "react/prop-types": 0,
+      "react/no-unescaped-entities": 0,
+      "semi": "error",
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+      "linebreak-style": ["error", "unix"],
+      "no-trailing-spaces": "error",
+    }
+};
